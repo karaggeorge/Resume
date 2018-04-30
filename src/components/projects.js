dddc049746a07807293a510d8dbbc6ab349fae1e
George Karagkiaouris <karaggeorge0@gmail.com>
2018-04-30 09:35:52 -0400

    Add projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -33,6 +33,58 @@ class Projects extends Component {
               <a className='github-btn' href='https://github.com/karaggeorge/react-better-password' target="_blank">View on GitHub</a>
             </div>
           </div>
+          <div className='project-card'>
+            <h1>Smart Checklist</h1>
+            <div className='card-body'>
+              <p>A smart checklist that provides guidance to process performers, is context-dependent and handles exceptional situations by dynamically updating the process steps. Mainly used for medical processes.</p>
+              <p>Capstone project based on <a href='http://laser.cs.umass.edu/techreports/15-021.pdf' target='_blank'>research</a> done at the University of Massachusetts Amherst (UMass).</p>
+              <p>This implementation succeeded in allowing multiple agents participating at once, something that hadn't been achieved so far. It is still being used for demos by the UMass team.</p>
+            </div>
+            <div className='btn-group'>
+              <a className='github-btn' href='http://capture.quinnipiac.edu/recordings/GKaragkiaouris/SER_Final_Demo/SER_Final_Demo_-_20170503_134924_15.html' target="_blank">Demo Presentation</a>
+              <a className='github-btn' href='https://github.com/karaggeorge/SmartChecklist' target="_blank">View on GitHub</a>
+            </div>
+          </div>
+          <div className='project-card'>
+            <h1>Messenger Socket App</h1>
+            <div className='card-body'>
+              <p>A group messenger app built in Java using Swing and awt for the graphics, while using Sockets and Threads for the connections.</p>
+              <p>Supports a group chat, connected user list and encrypted private messaging.</p>
+            </div>
+            <a className='github-btn' href='https://github.com/karaggeorge/MessengerSocketApp' target="_blank">View on GitHub</a>
+          </div>
+          <div className='project-card'>
+            <h1>Hangman</h1>
+            <div className='card-body'>
+              <p>A Hangman game built in Java using Swing and awt.</p>
+              <p>Features a big dictionary of words, various difficulties and lives settings.</p>
+            </div>
+            <a className='github-btn' href='https://github.com/karaggeorge/hangman' target="_blank">View on GitHub</a>
+          </div>
+          <div className='project-card'>
+            <h1>Space Ships</h1>
+            <div className='card-body'>
+              <p>A Space Ship 2D shooter game built in Java using Swing and awt.</p>
+              <p>Features sound effects, cursor aim, controls and animated achievement messages.</p>
+            </div>
+            <a className='github-btn' href='https://github.com/karaggeorge/SpaceShips' target="_blank">View on GitHub</a>
+          </div>
+          <div className='project-card'>
+            <h1>Maze Game</h1>
+            <div className='card-body'>
+              <p>A simple Maze game built in Java using Swing and awt.</p>
+              <p>Features challenging gameplay, textures and customizable coloration.</p>
+            </div>
+            <a className='github-btn' href='https://github.com/karaggeorge/MazeGame' target="_blank">View on GitHub</a>
+          </div>
+          <div className='project-card'>
+            <h1>DuckLamma</h1>
+            <div className='card-body'>
+              <p>A simple fast-paced game built in Java using Swing and awt.</p>
+              <p>Features fast-paced, engaging gameplay and a big collection of duck and lamma pictures</p>
+            </div>
+            <a className='github-btn' href='https://github.com/karaggeorge/DuckLamma' target="_blank">View on GitHub</a>
+          </div>
         </div>
         <h3>Contributions</h3>
         <div className='content'>
